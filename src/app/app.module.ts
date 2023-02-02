9066fed08a15b405e43a1f10dff332fadb4b23e2
awillsea <Alexanderwillsea@gmail.com>
2023-02-02 16:49:46 -0700

    tic-tac-toe app using firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { NbLayoutModule, NbThemeModule,NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    // NbThemeModule.forRoot({name:'cosmic'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    // NbThemeModule.forRoot({ name: 'cosmic' })
   ],
   providers: [],
   bootstrap: [AppComponent]
